ffa79c7401c58e62b164a0c655192384a9f7cf4b
BHAGAT233 <bhagatrammandavi24@navgurukul.org>
2025-05-23 22:19:56 +0530

    first commit

---
+++ 01_basic/test.js
@@ -0,0 +1,71 @@
+// let input = require('readline-sync');
+// let n = input.question('Enter the value: ')
+// console.log(n);
+// let b = n;
+// let r = 0;
+// let p = 0;
+// while(n!=0){
+//     r = n%10;
+//     p = p*10+r;
+//     n = Math.floor(n/10)
+// }
+// if(b == p){
+//     console.log('yes');
+
+// }
+// else{
+//     console.log('no');
+
+// }
+
+// const input = require('readline-sync');
+
+// let str = input.question('Enter a string: ');
+
+// // Reverse string
+// let reversed = str.split('').reverse().join('');
+
+
+// if (str === reversed) {
+//     console.log('Yes, it is a palindrome');
+// } else {
+//     console.log('No, it is not a palindrome');
+// }
+
+
+// let input = require('readline-sync');
+// let N = input.question('Enter the value: ')
+
+
+// let a = 1;
+// let b = 10;
+// let c = 1;
+// // let d = 2;
+
+// while(c<=b){
+//     console.log(`${c} x ${a} = ${a*c} : ${c} x ${2} = ${2*c} : ${c} x ${3} = ${3*c} : ${c} x ${4} = ${4*c}`);
+//     c++
+    
+// }
+
+
+
+// let user = {
+//     username: 'bhagat',
+//     price: 999,
+
+//     welcomeMesege : function (){
+//         console.log(`${this.username}welcome to website`);
+        
+//     }
+// }
+
+
+// let input = require('readline-sync');
+// let n = input.questionInt('enter the value: ')
+
+
+// let a = (n*n)**0.5;
+// console.log(a);
+
+
