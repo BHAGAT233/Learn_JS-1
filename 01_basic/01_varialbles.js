44805539855013d03b7c1cb752cfe0939f5acf2c
Bantu <bhagatrammandavi24@navgurukul.org>
2025-08-07 16:19:20 +0530

    added new project number guessing game

--- 01_basic/01_varialbles.js
+++ 01_basic/01_varialbles.js
@@ -1,17 +1,26 @@
-const accountId = 12345;
-let accountEmail = 'bantu@ng.com';
-var accountPassword = '4321'
-accountCity = 'jaipur'
+// const accountId = 12345;
+// let accountEmail = 'bantu@ng.com';
+// var accountPassword = '4321'
+// accountCity = 'jaipur'
 
-// accountId = '654'
+// // accountId = '654'
 
-accountEmail = 'bhagat@ng.com'
-accountPassword = '741852'
-accountCity = 'dheli'
+// accountEmail = 'bhagat@ng.com'
+// accountPassword = '741852'
+// accountCity = 'dheli'
 
-console.log(accountId);
-console.table([accountEmail,accountId,accountCity])
+// console.log(accountId);
+// console.table([accountEmail,accountId,accountCity])
 
+// ---------------------------------------------------------------------------//
 
+// let input = 'cat'
+// input = input.split('').sort().join('')
+// console.log(input);
 
+// if (input.length === 3 && input.split('').sort().join('') === 'act') {
+//     console.log(input);
+// } else {
+//     console.log('NO');
+// }
 
--- 06_dom/3.project/script.js
+++ 06_dom/3.project/script.js
@@ -1,6 +1,80 @@
-const randomNUmber = parseInt(Math.random() * 100 + 1)
+let randomNumber = parseInt(Math.random() * 100 + 1);
 
-let submit = document.querySelector('#subt')
-let userInput = document.querySelector('#guessField')
-let guessSlot = document.querySelector('.guesses')
-let remaining = document.querySelector('.lastResult')
+let submit = document.querySelector('#subt');
+let userInput = document.querySelector('#guessField');
+let guessSlot = document.querySelector('.guesses');
+let remaining = document.querySelector('.lastResult');
+let lowOrHi = document.querySelector('.lowOrHi');
+let startOver = document.querySelector('.resulparas');
+
+const p = document.createElement('p');
+
+let prevGuess = [];
+let numGuess = 1;
+let playGame = true;
+
+if (playGame) {
+    submit.addEventListener('click', function (e) {
+        e.preventDefault();
+        const guess = parseInt(userInput.value);
+        
+        if (isNaN(guess) || guess < 1 || guess > 100) {
+            alert('Please enter a number between 1 and 100');
+            return;
+        }
+
+        prevGuess.push(guess);
+        displayGuess(guess);
+        checkGuess(guess);
+    });
+}
+
+function checkGuess(guess) {
+    if (guess === randomNumber) {
+        displayMessage('🎉 You guessed it right!');
+        endGame();
+    } else if (numGuess === 11) {
+        displayMessage(`💀 Game Over. Random number was ${randomNumber}`);
+        endGame();
+    } else if (guess < randomNumber) {
+        displayMessage('📉 Number is too low');
+    } else if (guess > randomNumber) {
+        displayMessage('📈 Number is too high');
+    }
+}
+
+function displayGuess(guess) {
+    userInput.value = '';
+    guessSlot.innerHTML += `${guess}, `;
+    remaining.innerHTML = `${10 - numGuess}`;
+    numGuess++;
+}
+
+function displayMessage(message) {
+    lowOrHi.innerHTML = `<h2>${message}</h2>`;
+}
+
+function endGame() {
+    userInput.value = '';
+    userInput.setAttribute('disabled', '');
+    p.classList.add('button');
+    p.innerHTML = `<h2 id='newGame'>Start New Game</h2>`;
+    startOver.appendChild(p);
+    playGame = false;
+    newGame();
+}
+
+function newGame() {
+    const newGameButton = document.querySelector('#newGame');
+    newGameButton.addEventListener('click', function () {
+        randomNumber = parseInt(Math.random() * 100 + 1);
+        prevGuess = [];
+        numGuess = 1;
+        guessSlot.innerHTML = '';
+        remaining.innerHTML = `${10}`;
+        userInput.removeAttribute('disabled');
+        startOver.removeChild(p);
+        lowOrHi.innerHTML = '';
+        playGame = true;
+    });
+}
