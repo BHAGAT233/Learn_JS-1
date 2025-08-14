40964c724a96a59a840a44addd28cd03fe2d08ef
Bantu <bhagatrammandavi24@navgurukul.org>
2025-08-14 21:09:11 +0530

    add new

---
+++ Module_05/1.practice/script.js
@@ -0,0 +1,10 @@
+// let body = document.body
+// console.log(body);
+// let div = document.createElement('div')
+// // div.innerText = 'Hellow world'
+// // div.innerHTML = '<h1> Hello world 2 </h1>'
+// body.appendChild(div)
+
+
+
+let 
---
+++ Module_05/script.js
@@ -0,0 +1,26 @@
+// creat appendChild at div  
+
+// let body = document.body
+// console.log(body);
+// let div = document.createElement('div')
+// // div.innerText = 'Hellow world'
+// div.innerHTML = '<h1> Hello world 2 </h1>'
+// body.appendChild(div)
+
+
+// let div = document.querySelector('div')
+// console.log(div.textContent);
+// console.log(div.innerText);
+
+
+const body = document.body
+const div = document.querySelector('div')
+const sparHi = document.querySelector('#hi')
+const sparBye = document.querySelector('#bye')
+
+// sparBye.remove()
+// div.appendChild(sparBye)
+
+// div.replaceChild(sparHi)
+// sparHi.remove()
+console.log(sparHi.getAttribute('id'))
